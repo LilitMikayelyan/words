310963b457cea1f1c5b6c91e14be627301ecccf8
LilitMikayelyan <45179053+LilitMikayelyan@users.noreply.github.com>
2022-10-23 21:20:27 +0400

    Add files via upload

---
+++ words1.js
@@ -0,0 +1,36 @@
+var a = "hello world";
+var b = "hel world hell";
+var k = 0;
+var matrix = new Array(a.length+1);
+for (var i = 0; i < a.length+1; i++) {
+    matrix[i] = new Array(b.length);
+    k++;
+}
+for (let i = 0; i < b.length; i++) {
+      matrix[0][i] = 1;
+      k++;
+}
+var result = 0;
+var start = 0;
+for (let i = 1; i < a.length+1; i++) {
+
+    for (let j = 1; j < b.length; j++) {
+
+        if (a[i] == b[j])
+            matrix[i][j] = matrix[i - 1][j - 1] + 1;
+        else
+            matrix[i][j] = 0;
+        if (matrix[i][j] > result) {
+            result = matrix[+i][j];
+            start = j - matrix[i][j] + 1;
+        }
+        k++;
+    }
+}
+var word = "";
+for (let i = start ; i < start + result; i++) {
+    word += b[i];
+k++;
+}
+console.log(word);
+console.log(k);
